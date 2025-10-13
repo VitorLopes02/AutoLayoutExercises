feccac15c101ec8c06988a6f454e4b6af1d461ba
Vitor Lopes <vitinhoolopes.vl@gmail.com>
2025-10-13 19:51:59 -0300

    feat: add new tasks

---
+++ AutoLayoutTest/DesafioTituloFlexivel.swift
@@ -0,0 +1,29 @@
+//
+//  DesafioTituloFlexivel.swift
+//  AutoLayoutTest
+//
+//  Created by Vitor Lopes on 13/10/25.
+//
+
+import UIKit
+
+class DesafioTituloFlexivel: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
